696f5d1f4303bb6593d50230f14f4f010a99b209
yuanzhy <529164645@qq.com>
2022-05-01 11:38:03 +0800

    ByteUtil高低位存储调换

--- sqldog-server/src/main/java/com/yuanzhy/sqldog/server/util/ByteUtil.java
+++ sqldog-server/src/main/java/com/yuanzhy/sqldog/server/util/ByteUtil.java
@@ -21,30 +21,30 @@ public class ByteUtil {
 
     public static byte[] toBytes(short s) {
         byte[] bytes = new byte[2];
-        bytes[0] = (byte) (s & 0xff);
-        bytes[1] = (byte) ((s & 0xff00) >> 8);
+        bytes[0] = (byte) ((s & 0xff00) >> 8);
+        bytes[1] = (byte) (s & 0xff);
         return bytes;
     }
 
     public static byte[] toBytes(int n) {
         byte[] b = new byte[4];
-        b[0] = (byte) (n & 0xff);
-        b[1] = (byte) (n >> 8 & 0xff);
-        b[2] = (byte) (n >> 16 & 0xff);
-        b[3] = (byte) (n >> 24 & 0xff);
+        b[0] = (byte) (n >> 24 & 0xff);
+        b[1] = (byte) (n >> 16 & 0xff);
+        b[2] = (byte) (n >> 8 & 0xff);
+        b[3] = (byte) (n & 0xff);
         return b;
     }
 
     public static byte[] toBytes(long n) {
         byte[] b = new byte[8];
-        b[0] = (byte) (n & 0xff);
-        b[1] = (byte) (n >> 8 & 0xff);
-        b[2] = (byte) (n >> 16 & 0xff);
-        b[3] = (byte) (n >> 24 & 0xff);
-        b[4] = (byte) (n >> 32 & 0xff);
-        b[5] = (byte) (n >> 40 & 0xff);
-        b[6] = (byte) (n >> 48 & 0xff);
-        b[7] = (byte) (n >> 56 & 0xff);
+        b[0] = (byte) (n >> 56 & 0xff);
+        b[1] = (byte) (n >> 48 & 0xff);
+        b[2] = (byte) (n >> 40 & 0xff);
+        b[3] = (byte) (n >> 32 & 0xff);
+        b[4] = (byte) (n >> 24 & 0xff);
+        b[5] = (byte) (n >> 16 & 0xff);
+        b[6] = (byte) (n >> 8 & 0xff);
+        b[7] = (byte) (n & 0xff);
         return b;
     }
 
@@ -75,19 +75,19 @@ public class ByteUtil {
     }
 
     public static short toShort(byte[] bytes) {
-        return (short) toShort(bytes, 0);
+        return toShort(bytes, 0);
     }
 
     public static short toShort(byte[] bytes, int offset) {
-        return (short) ((0xff & bytes[offset++]) | (0xff00 & (bytes[offset++] << 8)));
+        return (short) ((0xff00 & bytes[offset++] << 8) | (0xff & (bytes[offset++])));
     }
 
     public static int toInt(byte[] bytes){
         return toInt(bytes, 0);
     }
 
-    public static int toInt(byte[] bytes, int offset){
-        return (0xff & bytes[offset++]) | (0xff00 & (bytes[offset++] << 8)) | (0xff0000 & (bytes[offset++] << 16)) | (0xff000000 & (bytes[offset++] << 24));
+    public static int toInt(byte[] bytes, int offset) {
+        return (0xff000000 & (bytes[offset++] << 24)) | (0xff0000 & (bytes[offset++] << 16)) | (0xff00 & (bytes[offset++] << 8)) | (0xff & bytes[offset++]);
     }
 
     public static long toLong(byte[] bytes) {
@@ -95,8 +95,11 @@ public class ByteUtil {
     }
     
     public static long toLong(byte[] bytes, int offset) {
-        return (0xffL & (long) bytes[offset++]) | (0xff00L & ((long) bytes[offset++] << 8)) | (0xff0000L & ((long) bytes[offset++] << 16)) | (0xff000000L & ((long) bytes[offset++] << 24))
-                | (0xff00000000L & ((long) bytes[offset++] << 32)) | (0xff0000000000L & ((long) bytes[offset++] << 40)) | (0xff000000000000L & ((long) bytes[offset++] << 48)) | (0xff00000000000000L & ((long) bytes[offset++] << 56));
+        return (0xff00000000000000L & ((long) bytes[offset++] << 56)) | (0xff000000000000L & ((long) bytes[offset++] << 48))
+                | (0xff0000000000L & ((long) bytes[offset++] << 40)) | (0xff00000000L & ((long) bytes[offset++] << 32))
+                | (0xff000000L & ((long) bytes[offset++] << 24)) | (0xff0000L & ((long) bytes[offset++] << 16))
+                | (0xff00L & ((long) bytes[offset++] << 8)) | (0xffL & (long) bytes[offset++])
+                ;
     }
 
     public static String toString(byte[] bytes) {
